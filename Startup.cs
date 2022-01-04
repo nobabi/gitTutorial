739a5610d6c7ff157d015aa83088767fdcb244f4
sherajul-Islam <sherajul529@gmail.com>
2022-01-04 11:29:44 -0800

    Revert "first work of git"
    This reverts commit 490218a83640b159beeec6fefce2d7310ecd710f.

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,6 @@ namespace gitTutorial
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            /*i have a git account*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
