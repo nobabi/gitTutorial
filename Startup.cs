477d90c4f6c0d60f8316658f62e1f5cd79b7396f
sherajul-Islam <sherajul529@gmail.com>
2022-01-04 11:31:14 -0800

    KK

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,9 @@ namespace gitTutorial
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // This method gets called by the runtime. Use this method to add services to the container.
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
