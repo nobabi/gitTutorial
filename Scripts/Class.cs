8add897251c04b9b8e1d3178a520c2fd97d10cb2
mdkabirhassan <87269678+mdkabirhassan@users.noreply.github.com>
2022-01-04 15:53:47 +0600

    DD

--- Scripts/Class.cs
+++ Scripts/Class.cs
@@ -7,5 +7,10 @@ namespace gitTutorial.Scripts
 {
     public class Class
     {
+        public int Address { get; set; }
+        public int Phone { get; set; }
+        public int Mail { get; set; }
+        public int Education { get; set; }
+
     }
 }
