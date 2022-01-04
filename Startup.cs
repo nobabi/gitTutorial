490218a83640b159beeec6fefce2d7310ecd710f
sherajul-Islam <sherajul529@gmail.com>
2022-01-04 11:25:22 -0800

    first work of git

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace gitTutorial
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            /*i have a git account*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
